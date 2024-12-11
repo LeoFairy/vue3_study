7a0f65b4678dec32aa4fb8b3eb2a30685056353f
ysf <1142669510@qq.com>
2024-12-11 13:25:50 +0800

    ref创建对象类型的响应式数据

--- 学习内容/09_ref创建对象类型的响应数据/components/Person.vue
+++ 学习内容/09_ref创建对象类型的响应数据/components/Person.vue
@@ -2,6 +2,12 @@
   <div class="person">
     <h2>一辆{{ car.brand }} 价值{{ car.price }}万</h2>
     <button @click="changePrice">修改汽车的价格</button>
+    <br>
+    <h2>游戏列表：</h2>
+    <ul>
+      <li v-for="game in games" :key="game.id">{{ game.name }}</li>
+    </ul>
+    <button @click="changeFirstGame"> 修改第一个游戏的名字</button>
   </div>
 </template>
 
@@ -12,10 +18,18 @@ let car = ref({
   brand: '奔驰',
   price: 100
 })
+let games = ref([
+  { id: '001', name: '王者荣耀' },
+  { id: '002', name: '原神' },
+  { id: '003', name: '蛋仔派对' },
+])
 
 
 function changePrice() {
   car.value.price += 10
+}
+function changeFirstGame() {
+  games.value[0].name = '英雄联盟'
 }
 
 </script>

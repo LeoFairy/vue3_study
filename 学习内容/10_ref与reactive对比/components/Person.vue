a077981e76dfc068b2cbf765e2abcf8119c3c55f
ysf <1142669510@qq.com>
2024-12-11 13:41:33 +0800

    ref与reactive对比

--- 学习内容/10_ref与reactive对比/components/Person.vue
+++ 学习内容/10_ref与reactive对比/components/Person.vue
@@ -2,20 +2,34 @@
   <div class="person">
     <h2>一辆{{ car.brand }} 价值{{ car.price }}万</h2>
     <button @click="changePrice">修改汽车的价格</button>
+    <button @click="changeCar">修改汽车</button>
+    <h2>当前求和为：{{ sum }}</h2>
+    <button @click="changeSum">点我sum+1</button>
+
   </div>
 </template>
 
 <script setup lang="ts" name="Person123">
-import { ref } from 'vue';
+import { reactive, ref } from 'vue';
 
-let car = ref({
+//数据
+let car = reactive({
   brand: '奔驰',
   price: 100
 })
 
+let sum = ref(0)
 
+//方法
 function changePrice() {
-  car.value.price += 10
+  car.price += 10
+}
+function changeCar() {
+  // car = { brand: '奥拓', price: 1 } //重新分配以后不是响应式的了
+  Object.assign(car, { brand: '奥拓', price: 1 })
+}
+function changeSum() {
+  sum.value += 1
 }
 
 </script>
